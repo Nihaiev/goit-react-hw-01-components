786e990a8d94dd49e9f64181b381ede6473eca08
Ivan <nigaev.ivan@gmail.com>
2022-10-13 20:37:34 +0300

    add components

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+
+const colors = ['#4CC7F3', '#A13CF0', '#F02C6E', '#20B8C5', '#A13CF0'];
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s.list}>
+        {stats.map(({ id, label, percentage }, index) => (
+          <li className={s.item} key={id} style={{ background: colors[index] }}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
